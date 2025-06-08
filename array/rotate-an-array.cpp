63fcd9e27ad7ba799f7fd62b1abfed3d7ab91aba
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-06-08 20:26:36 +0530

    rotating-array-o(2n)

---
+++ array/rotate-an-array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k;
+    cin>>k;
+    k=k%arr.size();
+    reverse(arr.begin(),arr.begin()+k);
+    reverse(arr.begin()+k,arr.end());
+    reverse(arr.begin(),arr.end());
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
