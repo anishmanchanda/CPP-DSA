0f802c4d75e7be1739048e1cbc4a5e46fa2869e9
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-07-08 17:43:13 +0530

    random-swap-test

--- randoms/test.cpp
+++ randoms/test.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <utility>
 using namespace std;
 /*
 double areaSwitchCase(int ch, vector<double> a) {
@@ -15,5 +16,24 @@ double areaSwitchCase(int ch, vector<double> a) {
 */
 int main(){
     cout<<"hello world"<<endl;
+    int arr1[5]={1,2,3,4,5};
+    int arr2[5]={6,7,8,9,10};
+    for(int i=0;i<5;i++){
+      cout<<arr1[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<5;i++){
+      cout<<arr2[i]<<" ";
+    }
+    cout<<endl<<endl;
+    swap(arr1[4],arr2[4]);
+    swap(arr1[0],arr2[0]);
+    for(int i=0;i<5;i++){
+      cout<<arr1[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<5;i++){
+      cout<<arr2[i]<<" ";
+    }
     return 0;
 }
